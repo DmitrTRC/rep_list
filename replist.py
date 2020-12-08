92db2dcc48efde499519a84665f2c530198633c6
DmitrTRC <9984398@gmail.com>
2020-12-08 15:21:52 +0300

    Bug fixed

--- replist.py
+++ replist.py
@@ -1,8 +1,9 @@
 import requests
+
 username = input("Enter the github username:")
-request = requests.get('https://api.github.com/users/'+username+'/repos')
+request = requests.get("https://api.github.com/users/" + username + "/repos")
 response = request.json()
-for repo in range(0,len(json)):
-  print("Project Number:",repo +1, end=' ## ')
-  print("Project Name:",response[repo]['name'])
-  print("Project URL:",response [repo]['svn_url'],"\n")
+for repo in range(0, len(response)):
+    print("Project Number:", repo + 1, end=" ## ")
+    print("Project Name:", response[repo]["name"])
+    print("Project URL:", response[repo]["svn_url"], "\n")
