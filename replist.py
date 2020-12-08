0313a92d82cab8aa854b01bb58ef9c3814cc7279
Dmitry Morozov <9984398@gmail.com>
2020-12-08 23:13:18 +0300

    Bugs fixed

--- replist.py
+++ replist.py
@@ -4,7 +4,7 @@ Program retrive list of the github public repositories
 import requests
 
 
-DEFAULT_NAME = 'dmitrtrc'
+DEFAULT_NAME = "dmitrtrc"
 
 if not (username := input("Enter the github username:")):
     username = DEFAULT_NAME
@@ -13,6 +13,8 @@ request = requests.get("https://api.github.com/users/" + username + "/repos")
 response = request.json()
 print("Verion 0.3:")
 for index, record in enumerate(response):
+    if index and not index % 10:
+        input("Press Enter to continue...")
     print("Project Number:", index + 1, end=" ## ")
     print("Project Name:", record["name"])
     print("Project URL:", record["svn_url"], "\n")
